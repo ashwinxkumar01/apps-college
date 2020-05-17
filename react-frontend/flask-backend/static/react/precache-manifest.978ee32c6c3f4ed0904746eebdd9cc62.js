c9d3c0a079098a2042923cc69c509e2d401427b8
Ashwin Kumar <ashwinxkumar01@berkeley.edu>
2020-05-17 01:52:50 -0700

    finished initial repository

---
+++ react-frontend/flask-backend/static/react/precache-manifest.978ee32c6c3f4ed0904746eebdd9cc62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459143d156dea1767c34ade9a8f9a7d1",
+    "url": "/static/react/../../../../../templates/index.html"
+  },
+  {
+    "revision": "7c5aeac6a94a10758b10",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec829bd0073e9a4f802c",
+    "url": "/static/react/js/2.916cb700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.916cb700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5aeac6a94a10758b10",
+    "url": "/static/react/js/main.135a818a.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
