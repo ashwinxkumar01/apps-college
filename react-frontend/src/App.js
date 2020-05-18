ba208502c8e9b6fd64291dfbbdb629dcbb78c271
Amitesh <amitesh.sharma.2001@gmail.com>
2020-05-17 17:23:54 -0700

    live server should work

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> save this.
         </p>
         <p>My Lovely Token = {window.token}</p>
         <a
