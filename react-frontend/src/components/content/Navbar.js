00197f0e59a8770b8cd6bef48a9b47dca78d61b9
Amitesh <amitesh.sharma.2001@gmail.com>
2020-05-22 15:27:08 -0700

    added a search bar to the navBar at the top

--- react-frontend/src/components/content/Navbar.js
+++ react-frontend/src/components/content/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
-import { Navbar, Button, Nav } from "react-bootstrap";
+import { Navbar, Button, Nav, Form, FormControl } from "react-bootstrap";
 import '../../App.css';
 
 class NavBar extends React.Component {
   render() {
+    
+    const divStyle = {
+      width: '200px;'
+    }
     return (
       <Navbar
         bg="light"
@@ -15,6 +19,12 @@ class NavBar extends React.Component {
         <Button variant="outline-info" onClick={this.props.toggle}>
           <FontAwesomeIcon icon={faAlignLeft} />
         </Button>
+
+        <Form inline className="ml-5 w-100">
+          <Form.Control type="text" placeholder="Search" className="mr-0 w-75" style={divStyle} />
+          <Button variant="outline-success" className="mr-0 w-0">Search</Button>
+        </Form>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto" navbar>
