22b5f6ee4944be014a81ea78508ed1b38152cba3
Ashwin Kumar <ashwinxkumar01@berkeley.edu>
2020-05-21 19:06:23 -0700

    Updated README and created new Login page

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ Then, navigate to flask-backend and run python3 main.py.
 
 Ensure that:
 1. npm install 
-2. npm install react-router-dom 
+2. npm install react-router-dom
+3. yarn add @material-ui/core
+4. yarn add @material-ui/icons
+
+are executed in the react-frontend folder beforehand. 
 
 For Python:
 1. pip install flask
-2. pip install pyerbase
+2. pip install pyrebase
 3. pip install pyodbc
 4. (For Mac) pip install homebrew (Gdog don't worry about it)
 
 Install the ODBC Driver 17 to run the backend Database.
-
-are executed in the react-frontend folder beforehand. 
 
 An alternative method to running the app is calling npm start from the react-frontend folder, however, this will not integrate with the backend.
--- react-frontend/src/pages/Login.js
+++ react-frontend/src/pages/Login.js
@@ -1,63 +1,131 @@
 import React from 'react';
-import '../css/Login.css';
-import GoogleButton from 'react-google-button';
-import Nav from '../components/Nav';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            user: '',
-            password: ''
-        };
-        this.handleChangeUser = this.handleChangeUser.bind(this);
-        this.handleChangePass = this.handleChangePass.bind(this);
-    }
-
-    googleLogin() {
-        console.log('Google button Clicked!');
-    };
-     
-    handleChangeUser(event) {
-        this.setState({ user: event.target.value});
-    };
-
-    handleChangePass(event) {
-        this.setState({ password: event.target.value});
-    };
-     
-    login = () => {
-        if (this.state.user === '' || this.state.password === '') {
-            console.log('empty');
-        }
-        else {
-            console.log(this.state.user);
-            console.log(this.state.password);
-        }
-        console.log('button works');
-    };
-    
-    render() {
-        return (
-            <div className = "Login" >    
-                <Nav />
-                <div className="credentials">
-                <h1 className="header">Welcome Back</h1>
-                    <div className="email-password">
-                        <input className="email" type="text" placeholder="Email" 
-                        value={this.state.user} onChange={this.handleChangeUser} />
-                        <input className="password" type="text" placeholder="Password" 
-                        value={this.state.password} onChange={this.handleChangePass} />
-                    </div>
-    
-                    <div className="login-button-div">
-                        <button className="login-button" onClick={this.login}>Log in</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.huffingtonpost.com/2016-02-03-1454538177-6729202-1156952a9865492c403b26f277_008_SMU_MainQuadAerial.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
