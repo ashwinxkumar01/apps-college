37c7c554a5aace257b81a0211a71f5801b031085
Amitesh <amitesh.sharma.2001@gmail.com>
2020-06-11 23:00:35 -0700

    start of the individual page

---
+++ react-frontend/src/pages/Individual.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../css/Individual.css';
+
+class Individual extends Component {
+    render() {
+        return (
+            <h1>Hello World</h1>   
+        )
+    }
+}
+
+export default Individual;
