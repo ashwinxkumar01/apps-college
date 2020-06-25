40b3928da1f0a733f6d7e32a7b302815548192ca
Amitesh <amitesh.sharma.2001@gmail.com>
2020-06-24 18:00:06 -0700

    navbar update pt.2?

--- react-frontend/src/components/content/Navbar.js
+++ react-frontend/src/components/content/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import '../../App.css';
 import SearchBar from './SearchBar';
+import { IoMdContact } from "react-icons/io";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
