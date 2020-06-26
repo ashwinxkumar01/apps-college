f7ab3686b38e55cd4a1730358b7923b3a07c3d0b
Mihir Gupta <m08.in.us@gmail.com>
2020-06-25 23:49:27 -0700

    updates to essays.js

--- react-frontend/src/pages/Essays.js
+++ react-frontend/src/pages/Essays.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from '../components/content/Navbar';
 import '../css/Essays.css';
-
+import {Popover, OverlayTrigger} from 'react-bootstrap';
+import {Common, Coalition } from '../components/Popovers';
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Essays extends Component {
     constructor(props) {
@@ -14,6 +17,10 @@ class Essays extends Component {
         this.requiresCoalitionApp = this.requiresCoalitionApp.bind(this);
         this.requiresOnlyCoalition = this.requiresOnlyCoalition.bind(this);
         this.requiresOnlyCommon = this.requiresOnlyCommon.bind(this);
+        this.renderFirstHeader = this.renderFirstHeader.bind(this);
+        this.renderUC = this.renderUC.bind(this);
+        this.renderCommon = this.renderCommon.bind(this);
+
     }
 
     requiresUCApp() {
@@ -46,6 +53,30 @@ class Essays extends Component {
         return requires;
     }
 
+    renderPopup() {
+        var onlyCommon = this.requiresOnlyCommon();
+        var onlyCoalition = this.requiresOnlyCoalition();
+
+        if(onlyCommon && this.requiresCoalitionApp()) {
+            return(
+                <OverlayTrigger trigger="click" placement="right" overlay={Common} rootClose>
+                            <div><FontAwesomeIcon icon={faInfoCircle} style={{opacity: '60%'}}/></div>
+                </OverlayTrigger>
+            )
+        }
+        else if(onlyCoalition && this.requiresCommonApp()) {
+            return(
+                <OverlayTrigger trigger="click" placement="right" overlay={Coalition} rootClose>
+                            <div><FontAwesomeIcon icon={faInfoCircle} style={{opacity: '60%'}}/></div>
+                </OverlayTrigger>
+            )
+        }
+    }
+
+    calculateNumEssays() {
+        var num = 0;
+    }
+
 
     componentDidMount() {
         fetch("/essays", {
@@ -68,76 +99,105 @@ class Essays extends Component {
           });
     }
 
-    renderFirstHeader() {
-        var coalition = this.requiresCoalitionApp();
+    renderUC = () => {
         var uc = this.requiresUCApp();
-        var common = this.requiresCommonApp();
-
-
+        if(uc) {
+            return(
+                <div>
+                    <div className = "header-div">
+                        <h3>UC Application (4 of 8 Required)</h3>
+                    </div>
+                    <div className="essaytext">
+                        <p>1. Describe an example of your leadership experience in which you have positively influenced others, helped resolve disputes or contributed to group efforts over time.</p>
+                        <p>2. Every person has a creative side, and it can be expressed in many ways: problem solving, original and innovative thinking, and artistically, to name a few. Describe how you express your creative side.</p>
+                        <p>3. What would you say is your greatest talent or skill? How have you developed and demonstrated that talent over time?  </p>
+                        <p>4. Describe how you have taken advantage of a significant educational opportunity or worked to overcome an educational barrier you have faced.</p>
+                        <p>5. Describe the most significant challenge you have faced and the steps you have taken to overcome this challenge. How has this challenge affected your academic achievement?</p>
+                        <p>6. Think about an academic subject that inspires you. Describe how you have furthered this interest inside and/or outside of the classroom.</p>
+                        <p>7. What have you done to make your school or your community a better place?</p>
+                        <p>8. Beyond what has already been shared in your application, what do you believe makes you stand out as a strong candidate for admissions to the University of California?</p>
+                    </div>
+                </div>
+            )
+        }
     }
 
-    render() {
-        this.requiresCoalitionApp();
-        this.requiresUCApp();
-        this.requiresCommonApp();
-        this.requiresOnlyCoalition();
-        this.requiresOnlyCommon();
-        return(
+    renderCommon = () => {
+        var common = this.requiresCommonApp();
+        if(common) {
+            return (
+                <div>
+                    <div className = "header-div">
+                        <h3>Common Application (1 of 7 Required)</h3>
+                    </div>
+
+                    <div className="essaytext">
+                        <p>1. Some students have a background, identity, interest, or talent so meaningful they believe their application would be incomplete without it. If this sounds like you, please share your story.</p>
+                        <p>2. The lessons we take from obstacles we encounter can be fundamental to later success. Recount a time when you faced a challenge, setback, or failure. How did it affect you, and what did you learn from the experience?</p>
+                        <p>3. Reflect on a time when you questioned or challenged a belief or idea. What prompted your thinking? What was the outcome?</p>
+                        <p>4. Describe a problem you’ve solved or a problem you’d like to solve. It can be an intellectual challenge, a research query, an ethical dilemma — anything of personal importance, no matter the scale. Explain its significance to you and what steps you took or could be taken to identify a solution.</p>
+                        <p>5. Discuss an accomplishment, event, or realization that sparked a period of personal growth and a new understanding of yourself or others.</p>
+                        <p>6. Describe a topic, idea, or concept you find so engaging it makes you lose all track of time. Why does it captivate you? What or who do you turn to when you want to learn more?</p>
+                        <p>7. Share an essay on any topic of your choice. It can be one you’ve already written, one that responds to a different prompt, or one of your own design.</p>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    renderCoalition = () => {
+        var coalition = this.requiresCoalitionApp();
+
+        if(coalition) {
+            return(
+                <div>
+                    <div className = "header-div">
+                        <h3>Coalition Application (1 of 5 Required)</h3>
+                    </div>
+
+                    <div className = "essaytext" >
+                        <p>1. Tell a story from your life, describing an experience that either demonstrates your character or helped to shape it.</p>
+                        <p>2. Describe a time when you made a meaningful contribution to others in which the greater good was your focus. Discuss the challenges and rewards of making your contribution.</p>
+                        <p>3. Has there been a time when you’ve had a long-cherished or accepted belief challenged? How did you respond? How did the challenge affect your beliefs?</p>
+                        <p>4. What is the hardest part of being a student now? What’s the best part? What advice would you give a younger sibling or friend (assuming they would listen to you)?</p>
+                        <p>5. Submit an essay on a topic of your choice.</p>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    renderFirstHeader = () => {
+        return (
             <div>
-                <NavBar searchBarInUse={this.searchBarInUse} setSearch={this.setSearch} active="3"/>
                 <div className="title">
                     <h1>Your Essay Summary</h1>
                 </div>
+
                 <div className="required">
-                        <h3>You have <b>8</b> required prompts</h3>
+                    <h3>You have <b>8</b> required prompts.</h3>
                 </div>
+
+                {this.renderPopup()}
+
                 <div className = "subtitle">
                     <h2>General Essays</h2>
-                </div>
+                </div>    
+            </div>
 
-                <div className = "header-div">
-                    <h3>UC Application (4 of 8 Required)</h3>
-                </div>
+        )
+    }
 
-                <div className="essaytext">
-                    <p>1.  Describe an example of your leadership experience in which you have positively influenced others, helped resolve disputes or contributed to group efforts over time.</p>
-                    <p>2. Every person has a creative side, and it can be expressed in many ways: problem solving, original and innovative thinking, and artistically, to name a few. Describe how you express your creative side.</p>
-                    <p>3. What would you say is your greatest talent or skill? How have you developed and demonstrated that talent over time?  </p>
-                    <p>4. Describe how you have taken advantage of a significant educational opportunity or worked to overcome an educational barrier you have faced.</p>
-                    <p>5. Describe the most significant challenge you have faced and the steps you have taken to overcome this challenge. How has this challenge affected your academic achievement?</p>
-                    <p>6.  Think about an academic subject that inspires you. Describe how you have furthered this interest inside and/or outside of the classroom.</p>
-                    <p>7. What have you done to make your school or your community a better place?</p>
-                    <p>8. Beyond what has already been shared in your application, what do you believe makes you stand out as a strong candidate for admissions to the University of California?</p>
 
-                </div>
 
-                <div className = "header-div">
-                    <h3>Common Application (1 of 7 Required)</h3>
-                </div>
-
-                <div className="essaytext">
-                    <p>1. Some students have a background, identity, interest, or talent so meaningful they believe their application would be incomplete without it. If this sounds like you, please share your story.</p>
-                    <p>2. The lessons we take from obstacles we encounter can be fundamental to later success. Recount a time when you faced a challenge, setback, or failure. How did it affect you, and what did you learn from the experience?</p>
-                    <p>3. Reflect on a time when you questioned or challenged a belief or idea. What prompted your thinking? What was the outcome?</p>
-                    <p>4. Describe a problem you’ve solved or a problem you’d like to solve. It can be an intellectual challenge, a research query, an ethical dilemma — anything of personal importance, no matter the scale. Explain its significance to you and what steps you took or could be taken to identify a solution.</p>
-                    <p>5. Discuss an accomplishment, event, or realization that sparked a period of personal growth and a new understanding of yourself or others.</p>
-                    <p>6. Describe a topic, idea, or concept you find so engaging it makes you lose all track of time. Why does it captivate you? What or who do you turn to when you want to learn more?</p>
-                    <p>7. Share an essay on any topic of your choice. It can be one you’ve already written, one that responds to a different prompt, or one of your own design.</p>
-
-                </div>
-
-                <div className = "header-div">
-                    <h3>Coalition Application (1 of 5 Required)</h3>
-                </div>
-
-                <div className = "essaytext" >
-                    <p>1. Tell a story from your life, describing an experience that either demonstrates your character or helped to shape it.</p>
-                    <p>2. Describe a time when you made a meaningful contribution to others in which the greater good was your focus. Discuss the challenges and rewards of making your contribution.</p>
-                    <p>3. Has there been a time when you’ve had a long-cherished or accepted belief challenged? How did you respond? How did the challenge affect your beliefs?</p>
-                    <p>4. What is the hardest part of being a student now? What’s the best part? What advice would you give a younger sibling or friend (assuming they would listen to you)?</p>
-                    <p>5. Submit an essay on a topic of your choice.</p>
-                </div>
-
+    render() {
+        return(
+            <div>
+                <NavBar searchBarInUse={this.searchBarInUse} setSearch={this.setSearch} active="3"/>
+                {this.renderFirstHeader()}
+                {this.renderUC()}
+                {this.renderCommon()}
+                {this.renderCoalition()}
                 <div className = "subtitle">
                     <h2>Supplemental Essays</h2>
                 </div>
