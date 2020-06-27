4ee000fbc96633c1c0c9b00c4b8a674c8dceb5b9
nvanny <nvanny@ucsd.edu>
2020-06-27 04:38:21 -0700

    resizing works better now for individual

---
+++ flask-backend/static/react/precache-manifest.52bc5d675f6976d6b80a36cd726a5d22.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31bf020e6bd27f9f7df56523bd7b2dc",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "db51b267603ea39c2624",
+    "url": "/static/react/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "276a66da2399f31307e2",
+    "url": "/static/react/css/main.cf74e46d.chunk.css"
+  },
+  {
+    "revision": "db51b267603ea39c2624",
+    "url": "/static/react/js/2.2bda246f.chunk.js"
+  },
+  {
+    "revision": "fb568d669b612c10e09415b640dbeacb",
+    "url": "/static/react/js/2.2bda246f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276a66da2399f31307e2",
+    "url": "/static/react/js/main.12ea7c1e.chunk.js"
+  },
+  {
+    "revision": "12419bf4deb9ecb0b4b0",
+    "url": "/static/react/js/runtime-main.725b2ec0.js"
+  },
+  {
+    "revision": "cf68b2ddb5f582493f368b2f3b972f39",
+    "url": "/static/react/media/UCSDCampus.cf68b2dd.jpg"
+  },
+  {
+    "revision": "b619b9512233854a80fd5041cad58aa0",
+    "url": "/static/react/media/UCSDLogo.b619b951.png"
+  },
+  {
+    "revision": "914f5b55ea697489d45b031bec3ecaa5",
+    "url": "/static/react/media/UCSD_3.914f5b55.jpg"
+  },
+  {
+    "revision": "2e1ce79c4071edf92b10bc549a2cb765",
+    "url": "/static/react/media/download.2e1ce79c.png"
+  },
+  {
+    "revision": "cb0d32d02216821155cf8311db4f6e7b",
+    "url": "/static/react/media/rooftopping-san-francisco-bay-area.cb0d32d0.jpg"
+  },
+  {
+    "revision": "e4ac0c4f384e21629078d91bd532e748",
+    "url": "/static/react/media/wp2017235.e4ac0c4f.jpg"
+  }
+]);
